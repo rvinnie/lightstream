21be69221257fb15a6e46830db8b7a661834a3d7
Alexey Fedoseev <alexey.fedoseev@mostgames.org>
2023-06-05 11:07:30 +0300

    feat: add postgres pkg

---
+++ pkg/database/postgres/postgres.go
@@ -0,0 +1,41 @@
+package postgres
+
+import (
+	"context"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"net"
+	"net/url"
+)
+
+type DBConfig struct {
+	Username string
+	Password string
+	Host     string
+	Port     string
+	DBName   string
+}
+
+func NewConnPool(dbConfig DBConfig) (*pgxpool.Pool, error) {
+	databaseUrl := formUrl(dbConfig.Username, dbConfig.Password, dbConfig.Host, dbConfig.Port, dbConfig.DBName)
+	dbPool, err := pgxpool.New(context.Background(), databaseUrl)
+	if err != nil {
+		return nil, err
+	}
+
+	err = dbPool.Ping(context.Background())
+	if err != nil {
+		return nil, err
+	}
+
+	return dbPool, err
+}
+
+func formUrl(username, password, host, port, dbname string) string {
+	var u = url.URL{
+		Scheme: "postgres",
+		User:   url.UserPassword(username, password),
+		Host:   net.JoinHostPort(host, port),
+		Path:   dbname,
+	}
+	return u.String()
+}
